589ce18e72f9e9a99f0c5a43241e926c50e5ac43
vpcsilva <vpcx5@yahoo.com.br>
2014-12-18 18:11:45 +0000

    alterando estrutura de diretorios

---
+++ yearbook/models.py
@@ -0,0 +1,107 @@
+# -*- coding: utf-8 -*-
+from django.db import models
+from django.core import serializers
+
+
+# Create your models here.
+class Cargo(models.Model):
+    nome = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.nome
+
+class Funcao(models.Model):
+    nome = models.CharField(max_length=200)
+    data_inicio = models.DateField('data_inicio')
+    data_saida = models.DateField('data_saida')
+
+    
+    def __str__(self):
+        return self.nome
+
+class Unidade_Organizacional(models.Model):
+    nome = models.CharField(max_length=200)
+    sigla = models.CharField(max_length=200, blank=True)
+    parent = models.ForeignKey('self', null=True, blank=True, related_name='children')
+    telefone = models.CharField(max_length=30)
+    ramal = models.CharField(max_length=5)
+    localidade = models.CharField(max_length=300, blank=True)
+    localidade_sala = models.ForeignKey('Sala', null=True, blank=True)
+    email = models.CharField(max_length=100, blank=True, null=True)
+
+    def __str__(self):
+        if self.sigla is None:
+            return self.nome
+        return self.sigla
+
+    def get_parent(self):
+        return self.parent
+
+    def get_sons(self):
+        return Unidade_Organizacional.objects.filter(parent=self)
+
+    def get_salas(self):
+        return Sala.objects.filter(uorg=self)
+
+    def recuperar_unidades_raiz(self):
+        return Unidade_Organizacional.objects.filter(parent=None)
+
+
+
+class Sala(models.Model):
+    identificador = models.CharField(max_length=200)
+    predio = models.CharField(max_length=200)
+    andar = models.IntegerField()
+    uorg = models.ForeignKey(Unidade_Organizacional)
+
+    def __str__(self):
+        return self.identificador
+
+    def get_uorg(self):
+        return self.uorg
+
+
+class Pessoa(models.Model):
+    nome = models.CharField(max_length=200)
+    telefone = models.CharField(max_length=30)
+    ramal = models.CharField(max_length=5)
+    nascimento = models.DateField('data de nascimento')
+    foto = models.CharField(max_length=200, blank=True)
+    cargo = models.ForeignKey(Cargo)
+    email = models.CharField(max_length=100, null=True, blank=True)
+    
+    ferias_inicio = models.DateField('inicio das ferias', blank=True, null=True)
+    ferias_fim = models.DateField('fim das ferias', blank=True, null=True)
+
+    licenca_inicio = models.DateField('inicio licenca', null=True, blank=True)
+    licenca_fim = models.DateField('fim licenca', null=True, blank=True)
+
+    historico = models.ManyToManyField('Lotacao', related_name='lotacoes_anteriores', blank=True)
+
+    def __str__(self):
+        return self.nome
+
+
+class Lotacao(models.Model):
+
+    nome = models.CharField(max_length=200)
+    pessoa = models.ForeignKey(Pessoa)
+    uorg = models.ForeignKey(Unidade_Organizacional)
+    sala = models.ForeignKey(Sala, blank=True, null=True)
+    funcao = models.ForeignKey(Funcao, blank=True, null=True)
+
+    entrada = models.DateField('data de entrada', null=True, blank=True, auto_now_add=True)
+    afastamento = models.DateField('data de afastamento', null=True, blank=True)
+
+    
+    def __str__(self):
+        return self.nome
+
+    def get_lotacoes_uorg(self, unidade):
+        return Lotacao.objects.filter(uorg=unidade)
+
+
+class User(models.Model):
+    nome = models.CharField(max_length=200)
+    password = models.CharField(max_length=16)
+    uorg = models.ForeignKey(Unidade_Organizacional)
---
+++ yearbook/views.py
@@ -0,0 +1,142 @@
+# -*- coding: utf-8 -*-
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
+from django.template import RequestContext, loader
+from django.core import serializers
+from django.views import generic
+
+
+from yearbook.models import Unidade_Organizacional
+from yearbook.models import Pessoa
+from yearbook.models import Cargo
+from yearbook.models import Funcao
+from yearbook.models import Sala
+from yearbook.models import Lotacao
+
+
+import json
+
+
+
+# Create your views here.
+
+def index(request):
+	try:
+		uorg = Unidade_Organizacional()
+		parent_uorgs_list = uorg.recuperar_unidades_raiz()
+		return uorg_info(request, parent_uorgs_list[0].pk)
+
+	except Unidade_Organizacional.DoesNotExist:
+		raise Http404
+
+def pessoa_info(request, pessoa_id):
+
+	pessoa = Pessoa.objects.get(pk=pessoa_id)
+	lotacoes_object = Lotacao.objects.filter(pessoa=pessoa)
+	lotacoes = []
+	for lotacao in lotacoes_object:
+		lotacoes.append(lotacao.nome)
+
+
+	
+	# pjson = Pessoa_json(id=pessoa.pk,nome=pes.nome,cargo=pessoa.cargo, funcao=lotacao.funcao.nome)
+
+	dicionario_pessoa = {
+						'id' 	: pessoa.pk,
+						'nome' 	: pessoa.nome,
+						'cargo' : pessoa.cargo.nome,
+						'funcao': lotacoes
+						}
+
+	json_pessoa = json.dumps(dicionario_pessoa)
+	return HttpResponse(json_pessoa)
+	
+def todas_salas_uorg(uorg):
+	
+	uorg_sons = uorg.get_sons()
+	salas_vetor = uorg.get_salas()
+	salas_uorg = {uorg : salas_vetor}
+
+	for son in uorg_sons:
+		salas_uorg.update(todas_salas_uorg(son))
+
+	return salas_uorg
+
+def uorg_info(request, uorg_id):
+	uorg = get_object_or_404(Unidade_Organizacional, pk=uorg_id)
+
+	sub_uorgs = uorg.get_sons
+	lotacoes = Lotacao.objects.filter(uorg=uorg)
+	pessoas = []
+	for lotacao in lotacoes:
+		pessoa = lotacao.pessoa
+		pessoas.append(pessoa)
+
+	salas = Sala.objects.filter(uorg=uorg)
+
+	funcoes = Lotacao.objects.filter(uorg=uorg).exclude(funcao__isnull=True)
+
+	return render(request, 'yearbook/index.html', {
+			'uorg' 		: uorg,
+			'sub_uorgs'	: sub_uorgs,
+			'pessoas'	: pessoas,
+			'lotacoes'	: lotacoes,
+			'salas'		: salas,
+			'funcoes'	: funcoes,
+		})
+
+
+def salas_uorg(request, uorg_id):
+	uorg = Unidade_Organizacional.objects.get(pk=uorg_id)
+
+	salas = todas_salas_uorg(uorg)
+	return salas
+
+def recuperar_salas_autocomplete_get(request, sala_nome):
+	salas = list(Sala.objects.filter(identificador__istartswith=sala_nome)) #
+
+	json_salas = serializers.serialize('json', salas)
+
+	return HttpResponse(json_salas)
+
+def recuperar_salas_autocomplete(request):
+	# salas = list(Sala.objects.filter(identificador__istartswith=sala_nome)) #
+
+	# json_salas = serializers.serialize('json', salas)
+
+	# return HttpResponse(json_salas)
+	return None
+
+
+def recuperar_pessoas_autocomplete(request, pessoa_nome):
+	pessoas = list(Pessoa.objects.filter(nome__startswith=pessoa_nome))
+	json_pessoas = serializers.serialize('json', pessoas)
+	return HttpResponse(json_pessoas)
+
+def pessoas_uorg(request, uorg_id):
+	return None
+
+def login(request):
+	template = loader.get_template('yearbook/login.html')
+	context = RequestContext(request, {})
+	return HttpResponse(template.render(context))
+
+def salvar_pessoa(request, user_id):
+	return None
+
+def salvar_uorg(request, user_id):
+	return None
+
+def salvar_sala(request, user_id):
+	return None
+
+def salvar_lotacao(request, user_id):
+	return None
+
+
+
+
+
+
+
+	
